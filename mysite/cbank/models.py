599599e1bcc63ce47757a1e22271df485ee4e4d1
Constantine Fedenko <cfedenko@gmail.com>
2011-08-02 22:41:26 +0300

    Add bank account types

--- mysite/cbank/models.py
+++ mysite/cbank/models.py
@@ -7,7 +7,6 @@ from cbank.gencc import completed_number
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     
-    
 class BankAccountManager(models.Manager):
     def create_account(self, user):
         generator = Random()
@@ -18,17 +17,27 @@ class BankAccountManager(models.Manager):
         return account
 
 class BankAccount(models.Model):
+    BANK_ACCOUNT_TYPE_CHOICES = (
+        (u'CC', u'Credit card'),
+        (u'TR', u'Transactional'),
+        (u'DP', u'Time deposit'),
+    )
     user = models.ForeignKey(User)
     number = models.PositiveIntegerField(unique=True)
+    accounttype = models.CharField(max_length=2,
+                                   choices=BANK_ACCOUNT_TYPE_CHOICES)
     
     objects = BankAccountManager()
     
     def __unicode__(self):
         return u'%d' % self.number
         
+      
 class  Transaction(models.Model):
-    payer = models.ForeignKey(BankAccount, related_name='%(class)s_as_payer_set')
-    payee = models.ForeignKey(BankAccount, related_name='%(class)s_as_payee_set')
+    payer = models.ForeignKey(BankAccount, 
+                              related_name='%(class)s_as_payer_set')
+    payee = models.ForeignKey(BankAccount,
+                              related_name='%(class)s_as_payee_set')
     value = models.DecimalField(max_digits=19, decimal_places=2)
     details = models.TextField()
     datetime = models.DateTimeField(auto_now=True, auto_now_add=True)
